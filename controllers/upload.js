9a9324516b4e595c393190f7a4ebc51254cae923
onier9702 <onier0217@gmail.com>
2022-09-03 08:08:58 -0700

    Upload Controller User Schema Populate fixed

--- controllers/upload.js
+++ controllers/upload.js
@@ -93,7 +93,6 @@ const handleImageCloudinary = async(req, res = response) => {
         };
 
         const prod = await Product.findById(id)
-                                    .populate('user', 'name')
                                     .populate('category', 'name');
 
         res.status(200).json({
